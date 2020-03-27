74aad57ba2318cb2bfa404946f7da8a1b8d100f8
leo-rocha <leonardocar.dm@gmail.com>
2020-03-27 02:49:46 -0400

    Change firestore config file

--- source/index.js
+++ source/index.js
@@ -7,26 +7,21 @@ require("firebase/auth");
 require("firebase/firestore");
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDQM3f3eT0H_-JPfwovLMEfW5Y4aI9Rgek",
-    authDomain: "dungeonsanddragons-420d3.firebaseapp.com",
-    databaseURL: "https://dungeonsanddragons-420d3.firebaseio.com",
-    projectId: "dungeonsanddragons-420d3",
-    storageBucket: "dungeonsanddragons-420d3.appspot.com",
-    messagingSenderId: "147775965249",
-    appId: "1:147775965249:web:4bc1760352808188bf5ec0",
-    measurementId: "G-82H6GF09PR"
+  apiKey: "AIzaSyArwJ2aFBL430sEobFMvM58q9QdoEnTQ6k",
+  authDomain: "test-db-88088.firebaseapp.com",
+  databaseURL: "https://test-db-88088.firebaseio.com",
+  projectId: "test-db-88088",
+  storageBucket: "test-db-88088.appspot.com",
+  messagingSenderId: "370716807223",
+  appId: "1:370716807223:web:3e94fca51af0d9fe756fb2"
 };
   
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-const admin = require('firebase-admin');
-const functions = require('firebase-functions');
+let db = firebase.firestore();
 
-admin.initializeApp(functions.config().firebase);
-
-let db = admin.firestore();
-
+/*
 let docRef = db.collection('users').doc('alovelace');
 
 let setAda = docRef.set({
@@ -43,3 +38,4 @@ let setAlan = aTuringRef.set({
   'last': 'Turing',
   'born': 1912
 });
+*/
